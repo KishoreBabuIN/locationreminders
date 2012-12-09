0d921020d3f405f5a6d1566a774ec7af70bdaec7
Greg <gregpaton08@gmail.com>
2012-12-09 00:15:17 -0500

    [mod] added temporary code to test listview functionality

--- src/com/androidgroup/gbp/reminders/MainActivity.java
+++ src/com/androidgroup/gbp/reminders/MainActivity.java
@@ -1,15 +1,44 @@
 package com.androidgroup.gbp.reminders;
 
+import java.util.LinkedList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    
+    // layout
+    private ListView _lvTasks;
 
+    // methods
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _lvTasks = (ListView) findViewById(R.id.lvTasks);
+        
+        task_list = new TaskList();
+        tasks = task_list.get_tasks();
+        tasks.add(new Task("move"));
+        tasks.add(new Task("Buy Milk"));
+        tasks.add(new Task("add task"));
+        tasks.get(0).set_due_time(0, 6, 8, 12, 2012);
+        tasks.get(0).set_remind_time(37, 2, 1);
+        Log.i("OC", "Time: " + tasks.get(0).get_due_time_string());
+        Log.i("OC", "time: " + tasks.get(0).get_remind_time_string());
+        String[] names = task_list.get_names();
+        Log.i("OC", "name: " + names[0]);        
+        
+        task_list.sort_by_name();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
+                                                                android.R.layout.simple_list_item_1, 
+                                                                android.R.id.text1, 
+                                                                task_list.get_names());
+        _lvTasks.setAdapter(adapter);
     }
 
     @Override
@@ -17,4 +46,9 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    
+    // attributes
+    private LinkedList<Task> tasks;
+    private TaskList task_list;
 }
